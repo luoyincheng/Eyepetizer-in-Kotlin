72a810caab1569143945ce4fb061dc9d6876a934
luoyincheng <yincheng.luo@gmail.com>
2019-04-05 11:31:22 +0800

    .

--- app/src/main/java/com/tt/lvruheng/eyepetizer/utils/ImageLoadUtils.kt
+++ app/src/main/java/com/tt/lvruheng/eyepetizer/utils/ImageLoadUtils.kt
@@ -5,35 +5,13 @@ import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DecodeFormat
 import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.request.RequestOptions
 import com.tt.lvruheng.eyepetizer.R
 
 /**
  * Created by lvruheng on 2017/7/6.
  */
-class ImageLoadUtils {
-
-    fun getRequestQptions1(): RequestOptions = RequestOptions()
-            .diskCacheStrategy(DiskCacheStrategy.ALL)
-            .placeholder(R.drawable.ic_image_loading)
-            .centerCrop()
-            .error(R.drawable.ic_empty_picture)
-
-    fun getRequestQptions2(): RequestOptions = RequestOptions()
-            .placeholder(R.drawable.ic_image_loading)
-            .centerCrop()
-            .diskCacheStrategy(DiskCacheStrategy.ALL)
-            .error(R.drawable.ic_empty_picture)
-
-
-    companion object {
-        fun getRequestQptions1(): RequestOptions = RequestOptions()
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .placeholder(R.drawable.ic_image_loading)
-                .centerCrop()
-                .error(R.drawable.ic_empty_picture)
-                .i
-
+class ImageLoadUtils{
+    companion object{
         fun display(context: Context, imageView: ImageView?, url: String) {
             if (imageView == null) {
                 throw IllegalArgumentException("argument error")
@@ -43,14 +21,9 @@ class ImageLoadUtils {
                     .centerCrop()
                     .placeholder(R.drawable.ic_image_loading)
                     .error(R.drawable.ic_empty_picture)
-                    ..into(imageView)
-            Glide.with(context)
-                    .applyDefaultRequestOptions(getRequestQptions1)
-                    .load(url)
-                    .into(imageView)
+                    .crossFade().into(imageView)
         }
-
-        fun displayHigh(context: Context, imageView: ImageView?, url: String) {
+        fun displayHigh(context: Context, imageView: ImageView?, url: String){
             if (imageView == null) {
                 throw IllegalArgumentException("argument error")
             }
