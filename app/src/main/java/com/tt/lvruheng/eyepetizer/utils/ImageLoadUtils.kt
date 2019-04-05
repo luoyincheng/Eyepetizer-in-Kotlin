22f770c94d53da5ad7cac8d6470c2dcc38ded0ab
luoyincheng <yincheng.luo@gmail.com>
2019-04-05 11:43:47 +0800

    .

--- app/src/main/java/com/tt/lvruheng/eyepetizer/utils/ImageLoadUtils.kt
+++ app/src/main/java/com/tt/lvruheng/eyepetizer/utils/ImageLoadUtils.kt
@@ -5,50 +5,50 @@ import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DecodeFormat
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.bitmap.DrawableTransformation
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
 import com.tt.lvruheng.eyepetizer.R
 
 /**
  * Created by lvruheng on 2017/7/6.
  */
-class ImageLoadUtils{
+class ImageLoadUtils {
 
-    companion object{
+    companion object {
 
         fun getRequestQptions1(): RequestOptions = RequestOptions()
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .placeholder(R.drawable.ic_image_loading)
                 .centerCrop()
+                .placeholder(R.drawable.ic_image_loading)
                 .error(R.drawable.ic_empty_picture)
 
         fun getRequestQptions2(): RequestOptions = RequestOptions()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .format(DecodeFormat.PREFER_ARGB_8888)
                 .placeholder(R.drawable.ic_image_loading)
                 .centerCrop()
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .error(R.drawable.ic_empty_picture)
 
         fun display(context: Context, imageView: ImageView?, url: String) {
             if (imageView == null) {
                 throw IllegalArgumentException("argument error")
             }
-            Glide.with(context).load(url)
-                    .diskCacheStrategy(DiskCacheStrategy.ALL)
-                    .centerCrop()
-                    .placeholder(R.drawable.ic_image_loading)
-                    .error(R.drawable.ic_empty_picture)
-                    .crossFade().into(imageView)
+            Glide.with(context)
+                    .load(url)
+                    .apply(getRequestQptions1())
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .into(imageView)
         }
-        fun displayHigh(context: Context, imageView: ImageView?, url: String){
+
+        fun displayHigh(context: Context, imageView: ImageView?, url: String) {
             if (imageView == null) {
                 throw IllegalArgumentException("argument error")
             }
-            Glide.with(context).load(url)
+            Glide.with(context)
                     .asBitmap()
-                    .format(DecodeFormat.PREFER_ARGB_8888)
-                    .diskCacheStrategy(DiskCacheStrategy.ALL)
-                    .centerCrop()
-                    .placeholder(R.drawable.ic_image_loading)
-                    .error(R.drawable.ic_empty_picture)
+                    .load(url)
+                    .apply(getRequestQptions2())
                     .into(imageView)
         }
     }
